8fbea787edbae8ebee9c4c5f4dc388d7d6fd2032
Максим Шабельник <maks2015611@gmail.com>
2023-06-20 17:05:30 +0300

    Add block contacts

---
+++ src/contacts.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Contacts = () => {
+    return (
+        <div className="container contacts py-5 text-light">
+            <div className="row">
+                <div className="col-12">
+                    <h2 className="text-center mb-4">Как с нами связаться</h2>
+                </div>
+                <div className="col-md-4">
+                    <img src="https://via.placeholder.com/300" alt="Contact" className="img-fluid" />
+                </div>
+                <div className="col-md-8">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse enim, saepe tempore nemo totam ipsam deleniti laboriosam in laborum voluptatem accusamus dignissimos velit incidunt odit suscipit amet ad adipisci modi!
+                        Pariatur atque dolorem provident unde quod minima culpa animi velit architecto molestias earum facilis laboriosam doloremque itaque officiis maxime temporibus laborum voluptas placeat ducimus, debitis mollitia ipsa soluta eveniet. Quam.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Contacts;
