e3f2bacb97549dc3dec98466eec2589915f3a4ba
Максим Шабельник <maks2015611@gmail.com>
2023-06-23 12:17:54 +0300

    Change

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,12 @@ function Header() {
     return (
         <header className=" text-uppercase" style={{ background: '#1C2336' }}>
             <div className="container d-flex flex-wrap justify-content-center py-3 mb-4">
-                <span className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-uppercase text-decoration-none fs-4" style={{ color: '#E3E3E3' }} >Portofino</span>
+                <span className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-uppercase text-decoration-none fs-4" style={{
+                    color: '#E3E3E3',
+                    fontSize: '28px',
+                    fontWeight: 'bold'
+                }}
+                >Portofino</span>
                 <ul className="nav" >
                     <li className="nav-item" ><a href="#" className="nav-link active" aria-current="page" style={{ color: '#E3E3E3' }}>О нас</a></li>
                     <li className="nav-item"><a href="#" className="nav-link" style={{ color: '#E3E3E3' }}>Меню</a></li>
--- src/footer.js
+++ src/footer.js
@@ -6,8 +6,14 @@ function Footer() {
         <footer className="bg-light text-center text-lg-start" style={{ marginTop: 'auto' }}>
 
 
-            <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-                © 2020 MDBootstrap.com
+            <div className="text-center p-3" style={{ backgroundColor: '#1C2336' }}>
+                <span className='text-uppercase' style={{
+                    color: '#E3E3E3',
+                    fontSize: '28px',
+                    fontWeight: 'bold'
+                }}>
+                    Подваааааааал
+                </span>
             </div>
         </footer>
     );
